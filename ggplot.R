61234376c8ebc04474678d1367d3d3498a6dc24a
linsabrina1019 <linsabrina1019@users.noreply.github.com>
2020-01-02 12:17:25 +0800

    updated

--- ggplot.R
+++ ggplot.R
@@ -19,34 +19,34 @@ death_total <-c(death_103[23], death_104[23], death_105[23], death_106[23], deat
 df <- data.frame(year = c(103, 104, 105, 106, 107),
                  adopt_total, death_total)
 
-fileset$`107.csv` <- fileset$`107.csv`[-23,]
+data <- fileset$`107.csv`[-23,]
 
-ggplot(data = df) + 
+p1 <- ggplot(data = df) + 
   geom_line(aes(year, adopt_total, group = 1, colour = "認養死亡率`")) +
   geom_line(aes(year, death_total, group = 1, colour = '所內死亡率')) +
   labs(x = "年", y = "percentage")
 
-ggplot(data = fileset$`107.csv`) + 
+p2 <- ggplot(data) + 
   geom_bar(mapping = aes(x = reorder(縣市別, -animals_per_person), y = animals_per_person),
            stat = "identity") +
   labs(x = "縣市別", y = "照顧動物數/人")
 
-ggplot(data = fileset$`107.csv`) + 
+p3 <- ggplot(data) + 
   geom_bar(mapping = aes(x = reorder(縣市別, -在養佔可留容比例), y = 在養佔可留容比例),
            stat = "identity") +
   labs(x = "縣市別", y = "在養佔可留容比例")
 
-ggplot(data = fileset$`107.csv`) + 
+p4 <- ggplot(data) + 
   geom_bar(mapping = aes(x = reorder(縣市別, -total), y = total),
            stat = "identity") +
   labs(x = "縣市別", y = "在養數")
 
-ggplot(data = fileset$`107.csv`) + 
+p5 <- ggplot(data) + 
   geom_bar(mapping = aes(x = reorder(縣市別, -`reliability(%)`), y = `reliability(%)`),
            stat = "identity") +
   labs(x = "縣市別", y = "可靠度")
 
-ggplot(data = fileset$`107.csv`) + 
+p6 <- ggplot(data) + 
   geom_line(aes(縣市別, `adopt_threat(%)`, group = 1, colour = "認養死亡率`")) +
   geom_line(aes(縣市別, `death_threat(%)`, group = 1, colour = '所內死亡率')) +
   labs(x = "縣市別", y = "percentage")
