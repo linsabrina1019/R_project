1edae3185097badda6c96cc5554543b560d0d969
linsabrina1019 <linsabrina1019@users.noreply.github.com>
2020-01-02 14:29:56 +0800

    new

--- ggplot.R
+++ ggplot.R
@@ -2,16 +2,16 @@ library(dplyr)
 library(plyr)
 library(ggplot2)
 
-adopt_107 <- fileset$`107.csv`[5][[1]]
-adopt_106 <- fileset$`106.csv`[5][[1]]
-adopt_105 <- fileset$`105.csv`[5][[1]]
-adopt_104 <- fileset$`104.csv`[5][[1]]
-adopt_103 <- fileset$`103.csv`[5][[1]]
+adopt_107 <- fileset$`107.csv`[4][[1]]
+adopt_106 <- fileset$`106.csv`[3][[1]]
+adopt_105 <- fileset$`105.csv`[3][[1]]
+adopt_104 <- fileset$`104.csv`[3][[1]]
+adopt_103 <- fileset$`103.csv`[3][[1]]
 death_107 <- fileset$`107.csv`[6][[1]]
-death_106 <- fileset$`106.csv`[6][[1]]
-death_105 <- fileset$`105.csv`[6][[1]]
-death_104 <- fileset$`104.csv`[6][[1]]
-death_103 <- fileset$`103.csv`[6][[1]]
+death_106 <- fileset$`106.csv`[5][[1]]
+death_105 <- fileset$`105.csv`[5][[1]]
+death_104 <- fileset$`104.csv`[5][[1]]
+death_103 <- fileset$`103.csv`[5][[1]]
 
 adopt_total <- c(adopt_103[23], adopt_104[23], adopt_105[23], adopt_106[23], adopt_107[23])
 death_total <-c(death_103[23], death_104[23], death_105[23], death_106[23], death_107[23])
@@ -22,7 +22,7 @@ df <- data.frame(year = c(103, 104, 105, 106, 107),
 data <- fileset$`107.csv`[-23,]
 
 p1 <- ggplot(data = df) + 
-  geom_line(aes(year, adopt_total, group = 1, colour = "認養死亡率`")) +
+  geom_line(aes(year, adopt_total, group = 1, colour = "認領養率")) +
   geom_line(aes(year, death_total, group = 1, colour = '所內死亡率')) +
   labs(x = "年", y = "percentage")
 
@@ -47,6 +47,6 @@ p5 <- ggplot(data) +
   labs(x = "縣市別", y = "可靠度")
 
 p6 <- ggplot(data) + 
-  geom_line(aes(縣市別, `adopt_threat(%)`, group = 1, colour = "認養死亡率`")) +
+  geom_line(aes(縣市別, adopt, group = 1, colour = "認領養率")) +
   geom_line(aes(縣市別, `death_threat(%)`, group = 1, colour = '所內死亡率')) +
   labs(x = "縣市別", y = "percentage")
